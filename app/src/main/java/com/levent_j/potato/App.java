15d2ae973fc13c919dbbc37edb416361fefbaedc
Levent_J <liwenjing960110@gmail.com>
2016-10-07 10:47:48 +0800

    解决了sugar奇怪的bug

---
+++ app/src/main/java/com/levent_j/potato/App.java
@@ -0,0 +1,14 @@
+package com.levent_j.potato;
+
+import android.app.Application;
+
+/**
+ * Created by levent_j on 16-10-7.
+ */
+public class App extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
